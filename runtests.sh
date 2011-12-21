c96be8958dab8e877b9d8f46c806f19698319222
Luper Rouch <luper.rouch@gmail.com>
2011-12-21 21:19:46 +0100

    tests should pass regardless of settings now

--- runtests.sh
+++ runtests.sh
@@ -1,2 +1,2 @@
 coverage run --source fsfield test_project/manage.py test fsfield
-coverage report --omit="src/fsfield/tests*" 
+coverage report --omit="src/fsfield/tests*" -m
--- src/fsfield/tests/models.py
+++ src/fsfield/tests/models.py
@@ -8,9 +8,13 @@ from fsfield import FileStorageField
 tmp_dir = op.join(op.dirname(__file__), "tmp")
 storage = FileSystemStorage(tmp_dir)
 
+
 class StorageModel(models.Model):
 
     normal_field = models.CharField(max_length=255)
-    simple_file_field = FileStorageField()
-    json_file_field = FileStorageField(storage, load=json.load, dump=json.dump)
-    custom_name_file_field = FileStorageField(filename="custom")
+    simple_file_field = FileStorageField(storage=storage)
+    json_file_field = FileStorageField(load=json.load, dump=json.dump,
+            storage=storage)
+    custom_name_file_field = FileStorageField(filename="custom",
+            storage=storage)
+    default_storage_field = FileStorageField()
